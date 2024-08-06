43fe74dc91d0c09cb27e3b62339ec2eb46adb2a8
Camila Serantoni <camila.serantoni@blue.cl>
2024-08-06 08:19:58 -0400

    passing a value from a server component

--- app/dashboard/counter/page.tsx
+++ app/dashboard/counter/page.tsx
@@ -1,14 +1,20 @@
+import { CartCounter } from "@/app/components/shopping-cart/components/CartCounter";
+
+
+
+export const metadata = {
+ title: 'Shopping Cart',
+ description: 'simple contador',
+};
+
 
 export default function CounterPage() {
+
   return (
    <div className="flex flex-col items-center justify-center w-full h-full" >
 <span> Productos en carrito de compras </span>
-<span className="text-9xl"> 10</span>
-   
-   <div>
-    <button className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2">+1</button>
-    <button></button>
-   </div>
+<CartCounter value={20}/>
+
    
    </div>
    
