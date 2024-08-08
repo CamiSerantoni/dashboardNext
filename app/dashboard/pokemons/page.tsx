46ec352b22e4943de0856aead0cb96882e97bd8c
Camila Serantoni <camila.serantoni@blue.cl>
2024-08-07 21:57:53 -0400

    bringing my first image pokemon

--- app/dashboard/pokemons/page.tsx
+++ app/dashboard/pokemons/page.tsx
@@ -1,16 +1,31 @@
+import { PokemonsResponse } from '@/app/pokemons';
+import { SimplePokemon } from '../../pokemons/interfaces/simple-pokemon';
+import Image from 'next/image';
 
-const getPokemons = async (limit= 20, offset= 0) => {
+const getPokemons = async (limit= 20, offset= 0) :Promise <SimplePokemon[]> => {
   const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`);
-  const data = await response.json();
-  return data;
+  const data:PokemonsResponse = await response.json();
+ 
+ const pokemons = data.results.map(pokemon => ({
+  id: pokemon.url.split('/').at(-2)!,
+  name: pokemon.name,
+ }))
+  return pokemons;
 }
 
 export default async function PokemonsPage() {
   const pokemons = await getPokemons(151);
 
   return (
-    <div>
-      {JSON.stringify(pokemons)}
+    <div className='flex flex-col'>
+    <div className='flex flex-wrap  gap-10 items-center justify-center'>
+      <Image 
+      src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/10.svg`}
+      width={100}
+      height={100}
+      alt="nombre"
+      />
+    </div>
     </div>
   );
 }
