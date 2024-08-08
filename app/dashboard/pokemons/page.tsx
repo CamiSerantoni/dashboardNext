b8b7ae308e853c2fde03015c70be020d59ebdcab
Camila Serantoni <camila.serantoni@blue.cl>
2024-08-07 22:06:50 -0400

    bringing dinamic pokemons

--- app/dashboard/pokemons/page.tsx
+++ app/dashboard/pokemons/page.tsx
@@ -15,16 +15,28 @@ const getPokemons = async (limit= 20, offset= 0) :Promise <SimplePokemon[]> => {
 
 export default async function PokemonsPage() {
   const pokemons = await getPokemons(151);
+  console.log(pokemons);
 
   return (
     <div className='flex flex-col'>
     <div className='flex flex-wrap  gap-10 items-center justify-center'>
-      <Image 
-      src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/10.svg`}
+{
+pokemons.map(pokemon => (
+   <Image 
+   key={pokemon.id}
+      src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`}
       width={100}
       height={100}
-      alt="nombre"
-      />
+      alt={pokemon.name}
+      />)
+
+)
+}
+
+
+
+
+   
     </div>
     </div>
   );
