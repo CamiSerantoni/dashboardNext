e73c1bfab62009235f6eaece2d37051886816e19
Camila Serantoni <camila.serantoni@blue.cl>
2024-08-09 22:45:48 -0400

    showing card and a beter view of each pokemon card

--- app/pokemons/components/PokemonGrid.tsx
+++ app/pokemons/components/PokemonGrid.tsx
@@ -10,14 +10,8 @@ export const PokemonGrid = ({pokemons}: Props) => {
     <div className='flex flex-wrap  gap-10 items-center justify-center'>
     {
     pokemons.map(pokemon => (
-        <PokemonCard key={pokemon.id}/>
-    //    <Image 
-    //    key={pokemon.id}
-    //       src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`}
-    //       width={100}
-    //       height={100}
-    //       alt={pokemon.name}
-    //       />)
+        <PokemonCard key={pokemon.id} pokemon={pokemon}/>
+      
     // <span key={pokemon.id}>asdasdasd</span>
     
     ))
