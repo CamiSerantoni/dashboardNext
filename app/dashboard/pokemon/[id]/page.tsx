ee5141e479519c7c4cd88caa9a3043e88057801e
Camila Serantoni <camila.serantoni@blue.cl>
2024-08-14 22:22:31 -0400

    starting with a pokemon

--- app/dashboard/pokemon/[id]/page.tsx
+++ app/dashboard/pokemon/[id]/page.tsx
@@ -1,15 +1,34 @@
+import { Pokemon } from "@/app/pokemons";
+
 
 interface Props {
     params: {id: string}
 }
+const getPokemon= async (id: string): Promise<Pokemon> => {   
 
-export default function PokemonPage({params} : Props) {
+const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
+    
+cache: "force-cache"// TODO: cambiar a futuro
+
+}).then(res => res.json());
+
+console.log('SE CARGÓ:', pokemon.name)
+return pokemon
+
+}
+
+
+export default async function PokemonPage({params} : Props) {
   
+    const pokemon = await  getPokemon(params.id)
 //   console.log({props})
   
     return (
     <div>
       <h1>Pokemon {params.id}</h1>
+      <div>
+        {JSON.stringify(pokemon)}
+      </div>
     </div>
   );
 }
