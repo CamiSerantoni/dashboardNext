e38d2db0fc55b920d64ef1af2d78403732ab9ad2
Camila Serantoni <camila.serantoni@blue.cl>
2024-09-16 08:38:31 -0300

    agregando counter slice

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
+import counterReducer from './counter/counterSlice'
+
+
 
 export const store = configureStore({
-  reducer: {
-    
-  },
+    reducer: {
+        counterReducer,
+    },
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
