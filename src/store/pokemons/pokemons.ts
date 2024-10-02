890fd347871c807d1335dc007d147cba0f1c780a
Camila Serantoni <camila.serantoni@blue.cl>
2024-10-01 23:52:59 -0300

    primeros pasos slice pokemons favorite

---
+++ src/store/pokemons/pokemons.ts
@@ -0,0 +1,27 @@
+import { SimplePokemon } from '@/src/pokemons';
+import { createSlice } from '@reduxjs/toolkit'
+
+/*
+{
+'1': {id:1, name: 'bulbasaur'},
+'2': {id:2, name: 'ivysaur'},}
+
+*/
+
+interface PokemonsState {
+    [key: string]: SimplePokemon
+}
+
+const initialState = {
+
+}
+
+const pokemonsSlice = createSlice({
+  name: second,
+  initialState,
+  reducers: {}
+});
+
+export const {} = pokemonsSlice.actions
+
+export default pokemonsSlice.reducer
