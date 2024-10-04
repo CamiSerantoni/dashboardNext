e36765922e1455b8e998c6f5ef4ea2918888f8b9
Camila Serantoni <camila.serantoni@blue.cl>
2024-10-03 22:43:37 -0300

    se agrega vista de no favoritos

--- src/app/dashboard/favorites/page.tsx
+++ src/app/dashboard/favorites/page.tsx
@@ -1,5 +1,6 @@
 import { SimplePokemon } from '@/src/pokemons';
 import { PokemonGrid, PokemonsResponse, FavoritePokemons } from '@/src/pokemons';
+import { IoHeadsetOutline, IoHeartOutline } from 'react-icons/io5';
 
 
 export const metadata = {
@@ -16,7 +17,17 @@ export default async function PokemonsPage() {
   return (
     <div className='flex flex-col'>
       <span className='text-5xl my-2 '>Pokémons favoritos <small className='text-blue-500'>Global State</small>  </span>
-      <FavoritePokemons />
+      {/* <FavoritePokemons /> */}
+      <NoFavorites/>
     </div>
   );
+}
+
+export  const NoFavorites =() => {
+  return (
+    <div className='flex flex-col h-[50vh] items-center justify-center'>
+      <IoHeartOutline size={100} className='text-red-500'  />
+      <span> No hay favoritos </span>
+    </div>
+  )
 }
--- src/pokemons/components/FavoritePokemons.tsx
+++ src/pokemons/components/FavoritePokemons.tsx
@@ -1,17 +1,20 @@
 'use client'
 
+import { useState } from 'react'
 import { PokemonGrid } from './PokemonGrid'
 import { useAppSelector } from '@/src/store'
 
 export const FavoritePokemons = () => {
 
-const favoritePokemons = useAppSelector( state => Object.values(state.pokemons))
 
+
+const favoritePokemons = useAppSelector( state => Object.values(state.pokemons))
+const [pokemons, setPokemons] = useState( favoritePokemons)
 // console.log(Object.values(favoritePokemons), 'FAVORRITES')
   return (
    
     <div>
-      <PokemonGrid pokemons={favoritePokemons}/>
+      <PokemonGrid pokemons={pokemons}/>
     </div>
   )
 }
